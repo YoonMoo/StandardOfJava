c939151036cdef60f3b3a24fb896d884853e0019
Yoonmo <momooy6929@gmail.com>
2022-07-04 22:37:51 +0900

    ch01 end

--- ch.01/src/ch01.java
+++ ch.01/src/ch01.java
@@ -1,5 +1,10 @@
-public class ch01 {
-     public static void main(String[] args){
-          System.out.println("hi");
+public class ch01 { // 소스파일의 이름은 클래스 명과 일치할 것, 하나의 클래스에는 하나의 public class만 두기
+     /*
+     1. javac.exe가 소스코드(.java)를 컴파일 ( 바이트코드(.class)로 변환 )
+     - c에서 바이너리코드 였다면, java에서는 jvm이 이해할 수 있는 바이트코드로 변환하는 것
+     2. java.exe가 클래스 파일을 실행
+      */
+     public static void main(String[] args){ // Java.exe가 호출 / args가 매개변수 이름.. 변경할 수 있었다.
+          System.out.println("2022-07-03 Java Start");
      }
 }
