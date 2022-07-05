bbce8a217c779514a78d42ec745eb008a8f2f780
Yoonmo <momooy6929@gmail.com>
2022-07-05 21:36:28 +0900

    ch.02 test

---
+++ ch.02_variable/src/ch02_variable.java
@@ -0,0 +1,12 @@
+public class ch02_variable {
+    public static void main(String[] args){
+        int year = 0;
+        int age = 28;
+
+        year = age + 2000;
+        age = age+1;
+
+        System.out.println(year);
+        System.out.println(age);
+    }
+}
