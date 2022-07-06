52856c62c1e5cee96f8090c61533733977919c9e
Yoonmo <momooy6929@gmail.com>
2022-07-06 22:52:18 +0900

    Quiz

---
+++ ch.02_variable/src/Quiz.java
@@ -0,0 +1,45 @@
+public class Quiz {
+    /*
+    1.
+    boolean -> 1byte
+    char -> 2byte
+    byte 1, short 2 , int 4,  long 8
+    float 4, double 8,
+
+    2.
+    long regNo = 0001011111111L;
+
+    3.
+    리터럴 : 100, 100L,f
+    변수 : i , l
+    키워드 : int, long, final, float
+    상수 : PI
+
+    4. b
+
+    5.
+    12
+    true
+    AB / 131
+    12 / 51
+    12 / 99
+    Java
+    오류
+
+    6. e / b,c,d,e -> 대문자 붙어서 키워드아님
+
+    7. a,g / a,d,e,g
+
+    8. a, d // 모든 변수는 4byte
+
+    9. d, e
+
+    10. 0 ~ 65535
+
+    11. a,b,c,d
+
+    12. a,b,c / e -> public과 static 위치 변경 가능
+
+    13. c, e / c,e,f -> String은 참조형 타입. 참조형 타입 기본값은 모두 null
+     */
+}
