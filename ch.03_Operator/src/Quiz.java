d0b1b86f4e544e97d10927b36e363a73eacc97a6
Yoonmo <momooy6929@gmail.com>
2022-07-10 22:26:50 +0900

    Quiz

---
+++ ch.03_Operator/src/Quiz.java
@@ -0,0 +1,45 @@
+public class Quiz {
+    public static void main(String[] args){
+        int x= 2, y=5;
+        char c = 'A';
+
+        System.out.println(y>=5 || x<0 && x>2);
+    }
+
+
+
+    /*
+    1.
+    6
+    true
+    13
+    5
+    false
+    2
+    5
+    66
+    B
+    B
+    C
+
+    2.
+    numOfApples/sizeOfBucket + (numApples%sizeofBucket > 0? 1:0
+
+    3.
+    num > 0 ? "양수" : (num < 0 ? "음수" : "0")
+
+    4.
+    num/100*100
+
+    5.
+    num/10*10+1
+
+    6.
+    1-num%10
+
+    7.
+    (int)((5/9f * (fahrenheit - 32))*100 + 0.5) / 100f;
+
+
+     */
+}
