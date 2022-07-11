971dd92033a9aa9da0e61acb320803a4e4fbc846
Yoonmo <momooy6929@gmail.com>
2022-07-11 19:58:25 +0900

    Quiz

--- ch.03_Operator/src/Quiz.java
+++ ch.03_Operator/src/Quiz.java
@@ -1,12 +1,4 @@
 public class Quiz {
-    public static void main(String[] args){
-        int x= 2, y=5;
-        char c = 'A';
-
-        System.out.println(y>=5 || x<0 && x>2);
-    }
-
-
 
     /*
     1.
