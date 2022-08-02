6ba045ca87a7dae96c8f2dc0a4d4c694bf61037e
Yoonmo <momooy6929@gmail.com>
2022-08-02 22:04:48 +0900

    Quiz 4-3)

---
+++ ch.04/src/Quiz.java
@@ -0,0 +1,34 @@
+public class Quiz {
+    public static void main(String[] args){
+        /*
+        4-1)
+        1. 10 < x && x < 20
+        2. ch == '' && ch!= '\t'
+        3. ch = 'x' || ch = 'X'
+        4. '0' <= ch && ch <= '9'
+        5. 'a' <= ch && ch <= 'Z' / 65 <= ch && ch <= 97
+        6. (year%400==0 || year%4==0) && year%100!=0
+        7. powerOn == false
+        8. str == "yes"
+
+        4-2)
+        int sum =0;
+        for(int i = 1 ; i <= 20 ; i++){
+            if(i%2== 0 || i%3 == 0) sum+=i;
+        }
+
+        4-3)
+        int sum, tmp =0;
+        for(int i = 0 ; i < 10; i++){
+            tmp = i+1;
+            sum += i + tmp;
+        }
+
+        4-4)
+        int sum, i = 0;
+        while(sum<=100){
+
+        }
+         */
+    }
+}
