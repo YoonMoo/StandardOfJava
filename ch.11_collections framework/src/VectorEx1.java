40ed3bfa3cf990456967fc57c3c50ddeae984ba7
Yoonmo <momooy6929@gmail.com>
2022-11-15 23:11:50 +0900

    VectorEx1

---
+++ ch.11_collections framework/src/VectorEx1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class VectorEx1 {
+    public static void main(String[] args){
+        Vector v = new Vector(5);
+        v.add("1");
+        v.add("2");
+        v.add("3");
+        print(v);
+
+        v.trimToSize(); // 빈공간 제거
+        System.out.println("=== After trimToSize () ===");
+        print(v);
+
+        v.ensureCapacity(6);
+        System.out.println("=== After ensureCapacity(6) ===");
+        print(v);
+
+        v.setSize(7);
+        System.out.println("=== After clear() ===");
+        print(v);
+    }
+
+    static void print(Vector v){
+        System.out.println(v);
+        System.out.println("size : " + v.size());
+        System.out.println("capacity : " + v.capacity());
+    }
+}
